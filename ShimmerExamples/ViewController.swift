f7212b757e24ee4657fd5560644e9ebb1181e548
Ömer Faruk Öztürk <omer.f.oztrk@gmail.com>
2021-01-08 22:34:03 +0300

    Setup project

---
+++ ShimmerExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShimmerExamples
+//
+//  Created by Ömer Faruk Öztürk on 8.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIView-Shimmer/UIView_Shimmer.h
@@ -0,0 +1,18 @@
+//
+//  UIView_Shimmer.h
+//  UIView-Shimmer
+//
+//  Created by Ömer Faruk Öztürk on 8.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIView_Shimmer.
+FOUNDATION_EXPORT double UIView_ShimmerVersionNumber;
+
+//! Project version string for UIView_Shimmer.
+FOUNDATION_EXPORT const unsigned char UIView_ShimmerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIView_Shimmer/PublicHeader.h>
+
+
